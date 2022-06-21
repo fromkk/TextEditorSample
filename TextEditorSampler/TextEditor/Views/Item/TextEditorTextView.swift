d9df7d5f8b51c44c40413c52498dd6c39dfb3d54
Kazuya Ueoka <k.ueoka@note.jp>
2022-06-21 18:54:33 +0900

    add default input view

---
+++ TextEditorSampler/TextEditor/Views/Item/TextEditorTextView.swift
@@ -0,0 +1,8 @@
+//
+//  TextEditorTextView.swift
+//  TextEditor
+//
+//  Created by Ueoka Kazuya on 2022/06/21.
+//
+
+import Foundation
---
+++ TextEditorSampler/TextEditor/Views/TextEditorItemView.swift
@@ -0,0 +1,8 @@
+//
+//  TextEditorItemView.swift
+//  TextEditor
+//
+//  Created by Ueoka Kazuya on 2022/06/21.
+//
+
+import Foundation
